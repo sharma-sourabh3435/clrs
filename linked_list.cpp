42477d7c7eaf583294819f610491833a0f3b88cd
Sourabh <sr925041@dal.ca>
2023-06-28 13:36:05 -0300

    chapter 10 initial

--- linked_list.cpp
+++ linked_list.cpp
@@ -1,5 +1,6 @@
 //
 // Created by SOURABH on 6/25/2023.
+// linked list implementation based on CLRS
 //
 
 #include "linked_list.h"
--- queue.cpp
+++ queue.cpp
@@ -1,5 +1,6 @@
 //
 // Created by SOURABH on 6/25/2023.
+// array based implementation of queue based on CLRS
 //
 
 #include "queue.h"
